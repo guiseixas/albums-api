e97daefc085e0a8570400194d62b4a9ee87f698a
guilherme <guilherme.santos@dellead.com>
2023-07-11 16:36:50 -0300

    first commit - project structure

---
+++ src/test/java/com/backend/albumsapi/AlbumsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.albumsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
